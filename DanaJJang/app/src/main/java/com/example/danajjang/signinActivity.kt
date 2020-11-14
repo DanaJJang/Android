dfdd61aedc0ca5665a8063189e6988e9229fb39d
dlswer23 <201114leb@dsm.hs.kr>
2020-11-14 23:05:40 +0900

    [ADD] 화면 Intent와 Toast추가

--- DanaJJang/app/src/main/java/com/example/danajjang/signinActivity.kt
+++ DanaJJang/app/src/main/java/com/example/danajjang/signinActivity.kt
@@ -3,11 +3,12 @@ package com.example.danajjang
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_signup.*
 import kotlinx.android.synthetic.main.fragment_addwordpad.*
 
-class signinActivity : AppCompatActivity() {
+class SigninActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
@@ -15,8 +16,11 @@ class signinActivity : AppCompatActivity() {
         //이제 회원가입이 완료되면 워드 패드를 추가하는 프레그 먼트로 이동한다.
 
         signbutton.setOnClickListener(){
-                val intent= Intent(this,addwordpad::class.java)
+                Toast.makeText(this,"가입이 완료되었습니다",Toast.LENGTH_SHORT).show()
+                val intent= Intent(this,MainActivity::class.java)
                 startActivity(intent)
+
+
             }
         }
 
