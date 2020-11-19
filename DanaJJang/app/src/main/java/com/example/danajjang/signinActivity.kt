721fa3b8efeafd9cae8ab1587fd833ee705a3198
dlswer23 <lbj77844>
2020-11-19 22:26:41 +0900

    [ADD]화면간 intent설정

--- DanaJJang/app/src/main/java/com/example/danajjang/signinActivity.kt
+++ DanaJJang/app/src/main/java/com/example/danajjang/signinActivity.kt
@@ -1,15 +1,24 @@
 package com.example.danajjang
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_signup.*
+import kotlinx.android.synthetic.main.fragment_addwordpad.*
 
 class signinActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
 
-        //이제 회원가입을 위한 창으로 넘어가는
+        //이제 회원가입이 완료되면 워드 패드를 추가하는 프레그 먼트로 이동한다.
+
+        signbutton.setOnClickListener(){
+                val intent= Intent(this,addwordpad::class.java)
+                startActivity(intent)
+            }
+        }
 
 
     }
-}
