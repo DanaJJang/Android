4dcc8b0f4262f289e2e5183477e22ff4da9faf9a
dlswer23 <201114leb@dsm.hs.kr>
2020-11-22 20:15:06 +0900

    [ADD] SignUpActivity
    혹시 내가 수정하다가 이상하게 될까봐 올림

---
+++ DanaJJang/app/src/main/java/com/example/danajjang/activity/SignUpActivity.kt
@@ -0,0 +1,51 @@
+package com.example.danajjang.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.danajjang.Auth
+import com.example.danajjang.NetRetrofit
+import com.example.danajjang.R
+import kotlinx.android.synthetic.main.activity_signup.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+
+        //이제 회원가입이 완료되면 워드 패드를 추가하는 프레그 먼트로 이동한다.
+
+        signup_btn.setOnClickListener(){
+            val auth = Auth(signup_user_id.text.toString(), signup_user_pw.text.toString())
+            Log.e("sdfa",auth.id)
+            Log.e("sdfa",auth.password)
+
+            val response: Call<Void> = NetRetrofit().getService().postSignUp(auth)
+
+            if(signup_user_pw.text.toString() == signup_user_pw_check.text.toString()){
+                response.enqueue(object: Callback<Void> {
+                    override fun onResponse(call: Call<Void>, response: Response<Void>) {
+                        if(response.code() == 200){
+                            Toast.makeText(applicationContext,"가입이 완료되었습니다",Toast.LENGTH_SHORT).show()
+                            val intent= Intent(applicationContext, SignInActivity::class.java)
+                            startActivity(intent)
+                        }
+                    }
+
+                    override fun onFailure(call: Call<Void>, t: Throwable) {
+                        Log.e("sdfa",t.message.toString())
+                    }
+
+                })
+            }else Toast.makeText(this,"비번 틀렸어요",Toast.LENGTH_SHORT).show()
+
+            }
+        }
+
+
+    }
