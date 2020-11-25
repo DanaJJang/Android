b3fbc0e63ff6be27539f1cb3e617c0cec849db0d
dlswer23 <201114leb@dsm.hs.kr>
2020-11-25 22:57:07 +0900

    [ADD] 공백이 아닐시에 버튼 보이기

---
+++ DanaJJang/app/src/main/java/com/example/danajjang/activity/AddWordPadActivity.kt
@@ -0,0 +1,22 @@
+package com.example.danajjang.activity
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import com.example.danajjang.R
+import kotlinx.android.synthetic.main.activity_addwordpad.*
+
+
+class AddWordPadActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_addwordpad)
+
+        addwordpad.setOnClickListener(){
+            val intent= Intent(this, ListActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+}
