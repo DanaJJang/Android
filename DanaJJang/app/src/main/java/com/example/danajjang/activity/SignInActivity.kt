6e5d2b2bff10993e93730c1121d724d672d62009
dlswer23 <201114leb@dsm.hs.kr>
2020-12-08 23:27:49 +0900

    [ADD] 로그인 api

--- DanaJJang/app/src/main/java/com/example/danajjang/activity/SignInActivity.kt
+++ DanaJJang/app/src/main/java/com/example/danajjang/activity/SignInActivity.kt
@@ -4,8 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.example.danajjang.API.Token
+import com.example.danajjang.API.Auth
+import com.example.danajjang.API.NetRetrofit
 import com.example.danajjang.R
 import kotlinx.android.synthetic.main.activity_signin.*
+import retrofit2.Call
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,11 +18,18 @@ class SignInActivity : AppCompatActivity() {
         Log.d("asdfdsaf","signin")
         setContentView(R.layout.activity_signin)
 
+        val sign = Auth(id.text.toString(),pw.text.toString())
+
+
+        val response : Call<Token> = NetRetrofit().getService().postSignIn(sign)
+
+
+
         button.setOnClickListener(){
            val intent= Intent(applicationContext, AddWordPadActivity::class.java)
             startActivity(intent)
         }
-        sign.setOnClickListener(){
+        signinword.setOnClickListener(){
             val intent= Intent(applicationContext, SignUpActivity::class.java)
             startActivity(intent)
         }
