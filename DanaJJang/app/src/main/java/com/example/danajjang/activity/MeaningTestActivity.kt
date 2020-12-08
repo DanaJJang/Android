0743e63cdd500b59eddf89229f3a0dddc09ed5c5
dlswer23 <201114leb@dsm.hs.kr>
2020-12-09 08:34:22 +0900

    [ADD] TestFile

---
+++ DanaJJang/app/src/main/java/com/example/danajjang/activity/MeaningTestActivity.kt
@@ -0,0 +1,13 @@
+package com.example.danajjang.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.danajjang.R
+
+class MeaningTestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_meaningtest)
+
+    }
+}
